72ccffe324d33ae6f1e9a664de99156e165b427d
xdudash <55356175+xdudash@users.noreply.github.com>
2026-09-14 16:50:36 +0200

    feat: load operations dashboard widgets

--- index.php
+++ index.php
@@ -49,6 +49,7 @@ $publicConfig = [
   <script src="assets/export.js"></script>
   <script src="assets/operations-automation.js"></script>
   <script src="assets/operations-command-center.js"></script>
+  <script src="assets/operations-dashboard.js"></script>
   <script>bootstrap();</script>
 </body>
 </html>
